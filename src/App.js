98439d81672e7aa4253fc2bf2bb067ddcb118966
Tushar <tushar789imran@gmail.com>
2022-12-11 19:43:50 +0600

    header is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import RootLayout from "./components/layout/RootLayout";
+
+let router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<RootLayout />}></Route>)
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/layout/RootLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Navbar from "./Navbar";
+
+function RootLayout() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+}
+
+export default RootLayout;
